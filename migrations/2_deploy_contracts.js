e37404d7d233117b94b30ae99e91bd30f39f60d2
leovs09 <leovs010@gmail.com>
2022-03-11 23:45:05 +0100

    truffle unbox drizzle

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
-const MyStringStore = artifacts.require("MyStringStore");
+const SimpleStorage = artifacts.require("SimpleStorage");
+const TutorialToken = artifacts.require("TutorialToken");
+const ComplexStorage = artifacts.require("ComplexStorage");
 
 module.exports = function(deployer) {
-  deployer.deploy(MyStringStore);
+  deployer.deploy(SimpleStorage);
+  deployer.deploy(TutorialToken);
+  deployer.deploy(ComplexStorage);
 };
