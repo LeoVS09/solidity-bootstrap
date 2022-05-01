391aeb5efa8553b29ba279889a29035a6a7dcc2a
leovs09 <leovs010@gmail.com>
2022-05-01 21:22:36 +0200

    truffle unbox DOkwufulueze/eth-vue

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,4 +1,7 @@
-var Migrations = artifacts.require("Migrations");
+"use strict";
+
+/* eslint-disable no-undef */
+const Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,8 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
-const TutorialToken = artifacts.require("TutorialToken");
-const ComplexStorage = artifacts.require("ComplexStorage");
+"use strict";
+
+/* eslint-disable no-undef */
+const DB = artifacts.require("./DB.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
-  deployer.deploy(ComplexStorage);
+  deployer.deploy(DB);
 };
